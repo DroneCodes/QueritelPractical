00ad0adc8f76cb2ba2f41ef78e73ac9732fdbc5e
Hp <oluwafisayomiogunyemi@gmail.com>
2022-08-04 01:28:12 +0100

    Fixed the Description Screen

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Queritel',
+      title: 'Queritel Practical',
       debugShowCheckedModeBanner: false,
       home: MainPage(),
     );
